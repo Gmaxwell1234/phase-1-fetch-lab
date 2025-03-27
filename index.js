02a11d8ba76580ff2e33b7409a12c2b1b1bb43a2
Maxwell <muriumaxwell1@gmail.com>
2025-03-27 14:14:23 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Return the fetch promise so tests can access it
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((jsonData) => renderBooks(jsonData));
 }
 
+function renderBooks(books) {
+  // This function is provided; it renders book titles to the DOM.
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
